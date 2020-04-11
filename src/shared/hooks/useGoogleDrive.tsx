e9156bca7ac48a247f939906d98833219db20336
Enzo Frantz <enzo.frantz@goeuro.com>
2020-04-12 00:10:09 +0200

    add google drive functionality for creating and reading files

--- src/shared/hooks/useGoogleDrive.tsx
+++ src/shared/hooks/useGoogleDrive.tsx
@@ -3,14 +3,9 @@ import React, { useState, useEffect, useContext, createContext } from "react";
 import { useGoogleApi } from "./useGoogleApi";
 import { useGoogleAuth } from "./useGoogleAuth";
 
-interface GoogleDriveFile {
-  name: string;
-  id: string;
-}
-
 interface GoogleDrive {
   fetchFiles?: () => void;
-  files?: GoogleDriveFile[];
+  files?: gapi.client.drive.File[];
   isFetchingFiles: boolean;
 }
 
@@ -18,28 +13,109 @@ const googleDriveContext = createContext<GoogleDrive>({
   isFetchingFiles: false,
 });
 
+// const createFile = async (name: string) => {
+//   const fileId = window.gapi.client.drive.files
+//     .create({
+//       resource: {
+//         name,
+//         parents: ["appDataFolder"],
+//         mimeType: "application/json",
+//         body: JSON.stringify({
+//           // @TODO verify this works
+//           hey: "you",
+//           you: 123,
+//         }),
+//       },
+//       fields: "id",
+
+//     })
+//     .then((response) => response.result.id);
+//   return fileId;
+// };
+
+const createFile = async (name: string, data: Object) => {
+  const boundary = "-------314159265358979323846";
+  const delimiter = "\r\n--" + boundary + "\r\n";
+  const close_delim = "\r\n--" + boundary + "--";
+
+  const contentType = "application/json";
+
+  const metadata = {
+    name: name,
+    mimeType: contentType,
+    parents: ["appDataFolder"],
+  };
+
+  const multipartRequestBody =
+    delimiter +
+    "Content-Type: application/json\r\n\r\n" +
+    JSON.stringify(metadata) +
+    delimiter +
+    "Content-Type: " +
+    contentType +
+    "\r\n\r\n" +
+    JSON.stringify(data) +
+    close_delim;
+
+  return await gapi.client.request({
+    path: "/upload/drive/v3/files",
+    method: "POST",
+    params: { uploadType: "multipart" },
+    headers: {
+      "Content-Type": 'multipart/related; boundary="' + boundary + '"',
+    },
+    body: multipartRequestBody,
+  });
+};
+
+const getFiles = async () => {
+  const files = await window.gapi.client.drive.files
+    .list({ spaces: "appDataFolder" })
+    .then((response) => response.result.files);
+  return files;
+};
+
+const getFileIdByName = async (name: string) => {
+  const files = await getFiles();
+
+  if (!files) {
+    return null;
+  }
+
+  const file = files.find((file) => file.name === name);
+
+  if (!file) {
+    return null;
+  }
+
+  return file.id;
+};
+
+const getFileContentByFileId = async (fileId: string) => {
+  const fileContent = await window.gapi.client.drive.files
+    .get({
+      fileId,
+      alt: "media",
+    })
+    .then((response) => response.result);
+  return fileContent;
+};
+
 export const useGoogleDriveProvider = () => {
   const { isGoogleApiReady } = useGoogleApi();
   const { isUserAuthenticated } = useGoogleAuth();
   const [isFetchingFiles, setIsFetchingFiles] = useState(false);
-  const [files, setFiles] = useState<GoogleDriveFile[]>([]);
+  const [files, setFiles] = useState<gapi.client.drive.File[]>([]);
 
-  const fetchFiles = () => {
+  const fetchFiles = async () => {
     if (!isGoogleApiReady || !isUserAuthenticated) {
       return Promise.reject();
     }
 
     setIsFetchingFiles(true);
-    window.gapi.client.drive.files
-      .list({
-        pageSize: 10,
-        fields: "nextPageToken, files(id, name)",
-      })
-      .then(function (response: any) {
-        setIsFetchingFiles(false);
-        const files: Array<GoogleDriveFile> = response.result.files;
-        setFiles(files);
-      });
+    const files = (await getFiles()) || [];
+    setIsFetchingFiles(false);
+    setFiles(files);
   };
 
   useEffect(() => {
